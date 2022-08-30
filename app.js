3df7437ae1ca810b2883fe0294aed240f01c1039
gnas-pham <minhkhangkg1@gmail.com>
2022-08-30 21:15:30 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const contactsRouter = require("./app/routes/contact.route");
 const cors = require("cors");
 
 const app = express();
@@ -9,4 +10,5 @@ app.use(express.json());
 app.get("/",(req,res) => {
     res.json({message: "Welcome to contact book applicotion."});
 });
+app.use("/api/contacts", contactsRouter);
 module.exports = app;
